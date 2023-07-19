2b3d2d6fef280553c926f2d4ac2d89e9a061160c
prathm patil <prathmpatil2818@gmail.com>
2023-07-19 08:57:52 +0530

    Completed Employee Payroll with React and Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pages from './Component/Pages/Pages';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Pages/>
+    </div>
+  );
+}
+
+export default App;
